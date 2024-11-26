7515a75091c807db708c9808a3f92f6bfd92dd35
Gilson de Oliveira <gmoj.mail@gmail.com>
2024-11-25 21:34:22 -0300

    flutter car project.

---
+++ asdfasdf/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:asdfasdf/pages/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+          centerTitle: true,
+        ),
+      ),
+      home: LoginPage(
+
+      ),
+    );
+  }
+}
